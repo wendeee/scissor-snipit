067e17cb3cf9ed76d909b7962313da83f9011e01
wendy <chinwe.promise2016@gmail.com>
2023-07-03 14:35:39 -0700

    fix: testing to see if rendering issue is fixed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 import App from "./App";
-// if (process.env.NODE_ENV === "production") disableReactDevTools();
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
